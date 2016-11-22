7903a098b0f7ac03b77c0ccd0b7a9a3c1d03fb7c
renato <rencarella.91@gmail.com>
2016-11-22 11:58:11 +0100

    kFold for binary data with AS

--- kFold_binary.py
+++ kFold_binary.py
@@ -13,16 +13,14 @@ gi = GeoIP.open("/usr/share/GeoIP/GeoLiteCity.dat", GeoIP.GEOIP_STANDARD)
 gi_asn = GeoIP.open("/usr/share/GeoIP/GeoIPASNum.dat", GeoIP.GEOIP_STANDARD)
 
 print("Reading data from csv...")
-df = pd.read_csv("dataset/dns_scan.csv", )
+df = pd.read_csv("dataset/dns_scan.csv", )  # inserire la scansione da cui prendere i dati, su github non ci sta per questioni di memoria
 ip_addresses = df.saddr
 print("...Done")
 
 n_col = 2
+n_row = 100000  # number of elements for the dataset
 
 # ------------------------------ Training Set ------------------------------
-
-n_row = 100000  # number of elements for training set
-
 
 X = np.empty(shape=[n_row * 2, n_col])
 
@@ -37,14 +35,8 @@ for i in range(n_row):
         asn = int(as_split[0].replace('AS', ""))
     else:
         asn = 0
-    # print(asn)
-
-    # X[i] = result
     X[i][0] = result
     X[i][1] = asn
-    # X[i][2] = latitude
-    # X[i][3] = longitude
-    # X[i][2] = region
 print("...Done")
 
 print("Generating some abnormal training data...")
@@ -52,27 +44,14 @@ for i in range(n_row):
     ip = '.'.join(map(str, ([random.randint(0, 255) for _ in range(4)])))
     address = ip.split(".")
     result = ''.join(map(str, ["{0:08b}".format(int(x)) for x in address]))
-    # gir = gi.record_by_addr(ip)
-    # if gir is not None:
-    #     latitude = gir['latitude'] + 60
-    #     longitude = gir['longitude'] + 60
-    # else:
-    #     latitude = 0
-    #     longitude = 0
     gio = gi_asn.org_by_addr(ip)
     if gio is not None:
         as_split = gio.split(' ')
         asn = int(as_split[0].replace('AS', ""))  # /65535
     else:
         asn = random.randint(0, 65535)
-    # print(asn)
-
-    # X[i + n_row] = result
     X[i + n_row][0] = result
     X[i + n_row][1] = asn
-    # X[i + n_row][2] = latitude
-    # X[i + n_row][3] = longitude
-    # X[i + n_row][2] = region
 print("...Done")
 
 # Labels
